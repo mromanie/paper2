c2ece1357796623e8444d0321216bda36f8befee
mromanie <mromanie@eso.org>
2021-10-27 22:28:34 +0200

    Add files via upload

--- ew_ldr.py
+++ ew_ldr.py
@@ -14,7 +14,7 @@ def deLatex(instring):
     :param instring: input string to be de-LaTeX-ed.
     :return: de-LaTex-ed string.
     """
-    return instring.replace('_', '').replace('{', '').replace('}', '')
+    return instring.replace('_', '').replace('{', '').replace('}', '').replace('$', '').replace('\\', '')
 
 
 def latex(instring, markups):
@@ -86,11 +86,12 @@ def select_in_common(ids, stellar_parameters, indices_in_common):
     return ids, stellar_parameters
 
 
-def plot_and_pick(ids, stellar_parameters_1, stellar_parameters_2, ylab, xlab):
-    fig, ((ax00, ax01), (ax10, ax11)) = plt.subplots(nrows=2, ncols=2, figsize=(12, 12))
+def plot_and_pick(ids, stellar_parameters_1, stellar_parameters_2, ylab, xlab, figsize):
+    fig, ((ax00, ax01), (ax10, ax11), (ax20, ax21)) = plt.subplots(nrows=3, ncols=2, figsize=figsize)
     fig.subplots_adjust(top=0.95, bottom=0.1, left=0.075, right=0.975)
-    fig.canvas.set_window_title('Left-click on a dot to highlight in all panels, '
-                                'middle- or right-click to move to analysing the spectrum of that object ...')
+    title = latex(ylab, '\mathrm')
+    fig.canvas.set_window_title('')
+    fig.suptitle(title)
     # https://stackoverflow.com/questions/7449585/how-do-you-set-the-absolute-position-of-figure-windows-with-matplotlib
 #     __, __, wdx, wdy = plt.get_current_fig_manager().window.geometry().getRect()
 #     plt.get_current_fig_manager().window.setGeometry(1100, 20, wdx, wdy)
@@ -98,28 +99,44 @@ def plot_and_pick(ids, stellar_parameters_1, stellar_parameters_2, ylab, xlab):
     line00, = ax00.plot(stellar_parameters_1['Teff']['value'],
                       stellar_parameters_1['Teff']['value'] - stellar_parameters_2['Teff']['value'],
                       marker='o', linestyle='', markersize=10, pickradius=5, picker=True)
-    ax00.set_xlabel(latex(stellar_parameters_1['Teff']['label'], '\mathrm') + ' ' + xlab)
-    ax00.set_ylabel(ylab)
+    ax00.set_xlabel(latex(stellar_parameters_1['Teff']['label'], '\mathrm') + ' ' + latex(xlab, '\mathrm'))
+    ax00.set_ylabel(r'$\Delta$' + latex(stellar_parameters_1['Teff']['label'], '\mathrm'))
     color, marker, = line00.get_color(), line00.get_marker()
     linestyle, markersize = line00.get_linestyle(), line00.get_markersize()
     #
     line01, = ax01.plot(stellar_parameters_1['Fe']['value'],
                       stellar_parameters_1['Fe']['value'] - stellar_parameters_2['Fe']['value'],
                       marker=marker, linestyle=linestyle, color=color, markersize=markersize, pickradius=5, picker=True)
-    ax01.set_xlabel(latex(stellar_parameters_1['Fe']['label'], '\mathrm') + ' ' + xlab)
-    ax01.set_ylabel(ylab)
+    ax01.set_xlabel(latex(stellar_parameters_1['Fe']['label'], '\mathrm') + ' ' + latex(xlab, '\mathrm'))
+    ax01.set_ylabel(r'$\Delta$' + latex(stellar_parameters_1['Fe']['label'], '\mathrm'))
     #
     line10, = ax10.plot(stellar_parameters_1['logg']['value'],
                       stellar_parameters_1['logg']['value'] - stellar_parameters_2['logg']['value'],
                       marker=marker, linestyle=linestyle, color=color, markersize=markersize, pickradius=5, picker=True)
-    ax10.set_xlabel(latex(stellar_parameters_1['logg']['label'], '\mathrm') + ' ' + xlab)
-    ax10.set_ylabel(ylab)
+    ax10.set_xlabel(latex(stellar_parameters_1['logg']['label'], '\mathrm') + ' ' + latex(xlab, '\mathrm'))
+    ax10.set_ylabel(r'$\Delta$' + latex(stellar_parameters_1['logg']['label'], '\mathrm'))
     #
     line11, = ax11.plot(stellar_parameters_1['vturb']['value'],
                       stellar_parameters_1['vturb']['value'] - stellar_parameters_2['vturb']['value'],
                       marker=marker, linestyle=linestyle, color=color, markersize=markersize, pickradius=5, picker=True)
-    ax11.set_xlabel(latex(stellar_parameters_1['vturb']['label'], '\mathrm') + ' ' + xlab)
-    ax11.set_ylabel(ylab)
+    ax11.set_xlabel(latex(stellar_parameters_1['vturb']['label'], '\mathrm') + ' ' + latex(xlab, '\mathrm'))
+    ax11.set_ylabel(r'$\Delta$' + latex(stellar_parameters_1['vturb']['label'], '\mathrm'))
+    #
+    line21, = ax20.plot(stellar_parameters_1['Teff']['value'] - stellar_parameters_2['Teff']['value'],
+                        stellar_parameters_1['Fe']['value'] - stellar_parameters_2['Fe']['value'],
+                        marker=marker, linestyle=linestyle, color=color, markersize=markersize, pickradius=5, picker=True)
+    ax20.set_xlabel(r'$\Delta$' + latex(stellar_parameters_1['Teff']['label'], '\mathrm'))
+    ax20.set_ylabel(r'$\Delta$' + latex(stellar_parameters_1['Fe']['label'], '\mathrm'))
+    #
+    bins = np.arange(-0.9, 0.2, 0.1)
+    ax21.hist(stellar_parameters_1['Fe']['value'], histtype=u'step', linewidth=3,
+                               label=latex(ylab.split('-')[0], '\mathrm'))
+    ax21.hist(stellar_parameters_2['Fe']['value'], bins=bins, histtype=u'step', linewidth=3,
+             label=latex(ylab.split('-')[-1], '\mathrm'))
+    ax21.set_xlabel(latex(stellar_parameters_1['Fe']['label'], '\mathrm'))
+    ax21.set_xlim(left=np.min(bins), right=np.max(bins))
+    ax21.legend(loc='upper left')
+
 
     index_picked = []
     def on_pick(event, figg):
@@ -139,7 +156,7 @@ def plot_and_pick(ids, stellar_parameters_1, stellar_parameters_2, ylab, xlab):
     plt.show()
 
 
-def main(what_plot):
+def main(what_plot, figsize):
     """
     Read the input files with the stellar parameters, match them, make the plots for the user to
     select one star from ... return the id of the selection
@@ -154,26 +171,33 @@ def main(what_plot):
     # Martino exc balance from 5500 K ... preferred
     ids_ew, stellar_parameters_ew = read_parameters('SH0ES_atmparam_FREETotal_alllines_all.dat')
     
-    xlab0 = r', $\mathrm{T}_\mathrm{eff}$ from '
-    if what_plot == 'ldr_ldr':
+    xlab0 = ', T_{eff}\ from\ '
+    if what_plot == 'ldr_ldrProx':
         ids = ids_ldr
         stellar_parameters_1 = stellar_parameters_ldr
         stellar_parameters_2 = stellar_parameters_ldrProx
-        xlab = xlab0 + r'$\mathrm{LDR}_\mathrm{us}$'
-        ylab = r'$\mathrm{LDR}_\mathrm{us} - \mathrm{LDR}_\mathrm{Proxauf}$'
+        xlab = xlab0 + 'LDR_{us}'
+        ylab = 'LDR_{us} - LDR_{Proxauf}'
     elif what_plot == 'ew_ldr':
         ids = ids_ew
         stellar_parameters_1 = stellar_parameters_ew
+        stellar_parameters_2 = stellar_parameters_ldr
+        xlab = xlab0 + 'EW'
+        ylab = 'EW - LDR_{us}'
+    elif what_plot == 'ew_ldrProx':
+        ids = ids_ew
+        stellar_parameters_1 = stellar_parameters_ew
         stellar_parameters_2 = stellar_parameters_ldrProx
-        xlab = xlab0 + r'$\mathrm{EW}$'
-        ylab = r'$\mathrm{EW} - \mathrm{LDR}_\mathrm{Proxauf}$'
+        stellar_parameters_2 = stellar_parameters_ldr
+        xlab = xlab0 + 'EW'
+        ylab = 'EW - LDR_{Proxauf}'
         
     else:
         print('Argument %s unknown, exiting ...' % (what_plot))
         sys.exit()
     
 
-    plot_and_pick(ids, stellar_parameters_1, stellar_parameters_2, ylab, xlab)
+    plot_and_pick(ids, stellar_parameters_1, stellar_parameters_2, ylab, xlab, figsize)
 
 
 # ______________________________________________________________________________________________________________________
@@ -182,4 +206,5 @@ if __name__ == '__main__':
 
 #     plt.style.use('Files/paper.mplstyle')
     what_plot = sys.argv[1]
-    index = main(what_plot)
+    figsize = (float(sys.argv[2]), float(sys.argv[3]))
+    index = main(what_plot, figsize)
